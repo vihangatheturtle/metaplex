955766effeb7de8970a452c64869720b565159ff
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2022-01-30 19:55:29 +0000

    added "Use CMID" button

--- js/packages/candy-machine-ui/src/cmInput.tsx
+++ js/packages/candy-machine-ui/src/cmInput.tsx
@@ -1,7 +1,16 @@
 export const CMInput = () => {
     return (
+      <div>
         <input
           placeholder="Enter CMID"
         />
+        <button id="selectCmid">
+          Use CMID
+        </button>
+      </div>
     );
 }
+
+document.getElementById('selectCmid').click(() => {
+  alert(0)
+});
