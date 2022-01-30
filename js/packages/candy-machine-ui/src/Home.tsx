ca3fcf94b5e03e21791a29d9622f13756bccec59
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2022-01-30 18:59:47 +0000

    ok

--- js/packages/candy-machine-ui/src/Home.tsx
+++ js/packages/candy-machine-ui/src/Home.tsx
@@ -164,7 +164,7 @@ const Home = (props: HomeProps) => {
   return (
     <Container style={{ marginTop: 100 }}>
       <Container maxWidth="xs" style={{ position: 'relative' }}>
-        <p>{this.props.candyMachineId}</p>
+        <p>{props.candyMachineId}</p>
         <Paper
           style={{ padding: 24, backgroundColor: '#151A1F', borderRadius: 6 }}
         >
