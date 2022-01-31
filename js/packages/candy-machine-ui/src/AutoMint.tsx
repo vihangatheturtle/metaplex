44320739027ceb0e3e54a4d6f0dd28a08e144893
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2022-01-31 01:11:42 +0000

    yesyes

--- js/packages/candy-machine-ui/src/AutoMint.tsx
+++ js/packages/candy-machine-ui/src/AutoMint.tsx
@@ -1,10 +1,11 @@
+import './AutoMint.css'
+
 export const AutoMintCheckbox = () => {
     return (
       <div>
         <input
           id="autoMintCheckbox"
           type="checkbox"
-          style="float: left; margin-right: 10px;"
         />
         <p>
           Auto Mint
