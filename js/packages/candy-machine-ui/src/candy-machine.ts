261546ad040bf41f4e31ab2abb84d5fbc242c956
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2022-02-01 23:17:53 +0000

    brhu

--- js/packages/candy-machine-ui/src/candy-machine.ts
+++ js/packages/candy-machine-ui/src/candy-machine.ts
@@ -248,7 +248,7 @@ export const mintMultipleTokens = async (
   candyMachine: CandyMachineAccount,
   payer: anchor.web3.PublicKey,
   amount: number,
-): Promise<(number | undefined)[]> => {
+) => {
   var done = 0;
   while (done < amount) {
     console.log("Started mint")
